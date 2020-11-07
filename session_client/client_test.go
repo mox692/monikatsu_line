6b56d36819cbf9bd583c1acbc001dd7449f83966
mox692 <55653825+mox692@users.noreply.github.com>
2020-11-07 16:54:43 +0900

    add client dir

---
+++ session_client/client_test.go
@@ -0,0 +1,25 @@
+package session_client
+
+import (
+	"monikatsuline/server"
+	"testing"
+)
+
+func Test_SetContext(t *testing.T) {
+
+	cases := map[string]server.SessionCode{
+		"etw":  server.MonikatsuFlag,
+		"teqw": server.MonikatsuSetWakeupTime,
+		"fds":  server.DefaultState,
+	}
+
+	for key, value := range cases {
+		status, err := SetContext(key, value)
+		if err != nil {
+			t.Errorf("error occured: %v\n", err)
+		}
+		if status.SetStatusCode != "0" {
+			t.Errorf("setstatuscode  is not collect: want %s, got %s\n", "0", status.SetStatusCode)
+		}
+	}
+}
