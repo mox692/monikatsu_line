f761c6ae995310bfc926c150759dfd913da35018
mox692 <55653825+mox692@users.noreply.github.com>
2020-11-07 22:15:04 +0900

    graceful shutdown

--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"time"
 
 	"github.com/line/line-bot-sdk-go/linebot"
 )
@@ -36,7 +40,21 @@ func main() {
 		}
 	})
 
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	s := http.Server{Addr: ":8080"}
+
+	go func() {
+		if err := s.ListenAndServe(); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt)
+	log.Printf("SIGNAL %d received, then shutting down...", <-quit)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	if err := s.Shutdown(ctx); err != nil {
 		log.Fatal(err)
 	}
 }
