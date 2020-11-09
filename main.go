af3512ba2b6af326b4db17f5538bdec2a01a1ac7
mox692 <55653825+mox692@users.noreply.github.com>
2020-11-09 19:12:23 +0900

    commit again

--- main.go
+++ main.go
@@ -3,46 +3,24 @@ package main
 import (
 	"context"
 	"log"
+	"monikatsuline/server"
 	"net/http"
 	"os"
 	"os/signal"
 	"time"
-
-	"github.com/line/line-bot-sdk-go/linebot"
 )
 
 const channel_secret = "c8654b200557eb6744b21fc78f240d0a"
 const channel_token = "G0k5a+PTDx8UU7s09xB3qD8viE7+AXGjrTJDBhiZ8Qs7j44nIKzlBgb0WFEqW7trLbJWcsX4HDXQJc3Bn+vJylb7S1sUnbzXiCWE9D4rUu9gsvYVVugCW4wc3dm6yQzlXsoPpBWIW4Kn1xZkOqRqjAdB04t89/1O/w1cDnyilFU="
 
 func main() {
-	bot, _ := linebot.New(channel_secret, channel_token)
 
-	http.HandleFunc("/callback", func(w http.ResponseWriter, req *http.Request) {
-		events, err := bot.ParseRequest(req)
-		if err != nil {
-			if err == linebot.ErrInvalidSignature {
-				w.WriteHeader(400)
-			} else {
-				w.WriteHeader(500)
-			}
-			return
-		}
-
-		for _, event := range events {
-			switch message := event.Message.(type) {
-			case *linebot.TextMessage:
-				resp := linebot.NewTextMessage(message.Text)
-				_, err := bot.ReplyMessage(event.ReplyToken, resp).Do()
-				if err != nil {
-					log.Print(err)
-				}
-			}
-		}
-	})
+	http.HandleFunc("/callback", server.JudgeEvent)
 
 	s := http.Server{Addr: ":8080"}
 
 	go func() {
+		log.Println("server runnning ...")
 		if err := s.ListenAndServe(); err != nil {
 			log.Fatal(err)
 		}
@@ -50,11 +28,11 @@ func main() {
 
 	quit := make(chan os.Signal, 1)
 	signal.Notify(quit, os.Interrupt)
-	log.Printf("SIGNAL %d received, then shutting down...", <-quit)
+	log.Printf("\nSIGNAL '%d' received, then shutting down...\n", <-quit)
 
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 	if err := s.Shutdown(ctx); err != nil {
-		log.Fatal(err)
+		log.Println(err)
 	}
 }
--- sessionClient/client_test.go
+++ sessionClient/client_test.go
@@ -1,4 +1,4 @@
-package session_client
+package sessionClient
 
 import (
 	"monikatsuline/server"
