9814020172209823180bbc3ef7ac382dab6aa13c
mox692 <55653825+mox692@users.noreply.github.com>
2020-11-26 17:27:46 +0900

    add exfile

--- database/connection.go
+++ database/connection.go
@@ -7,6 +7,7 @@ import (
 	"os"
 
 	_ "github.com/go-sql-driver/mysql"
+	"google.golang.org/appengine"
 )
 
 var Conn *sql.DB
@@ -14,16 +15,29 @@ var Conn *sql.DB
 func SetupDB() {
 
 	user := os.Getenv("DB_USER")
-	password := os.Getenv("DB_PASSWORD")
+	pass := os.Getenv("DB_PASS")
 	host := os.Getenv("DB_HOST")
 	port := os.Getenv("DB_PORT")
+	connectionName := os.Getenv("INSTANCE_CONNECTION_NAME")
 	database := os.Getenv("DB_DATABASE")
 	driverName := os.Getenv("DB_DRIVER")
 
 	var err error
-	Conn, err = sql.Open(driverName,
-		fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", user, password, host, port, database))
-	if err != nil {
-		log.Fatal(err)
+
+	switch appengine.IsAppEngine() {
+	case true:
+		cloudSQLConnection := user + ":" + pass + "@unix(/cloudsql/" + connectionName + ")/" + database + "?parseTime=true"
+		Conn, err = sql.Open("mysql", cloudSQLConnection)
+		if err != nil {
+			log.Println(err)
+			panic(err.Error())
+		}
+	case false:
+		Conn, err = sql.Open(driverName,
+			fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", user, pass, host, port, database))
+		if err != nil {
+			log.Println(err)
+			panic(err.Error())
+		}
 	}
 }
